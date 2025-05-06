643c25f48c9266ec7e3bc761f2e6e13f15cecb8b
David Connelly <davidc.webguy@gmail.com>
2025-05-06 15:48:38 +0100

    Added new documentation (broken into four separate 'books').

--- docs_ref_left_nav.php
+++
@@ -1,6 +0,0 @@
-<?php
-$sub_directories = $chapter['sub_directories'];
-$ref_chapter_root = $this->remove_first_four_if_numeric(url_title(REF_DIR));
-foreach($sub_directories as $sub_directory) {
-    echo '<li>'.anchor($ref_chapter_root.'/'.url_title($sub_directory['dir_name']), $sub_directory['dir_label']).'</li>';
-}
